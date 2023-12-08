3ee1d6f0f4a01fa9f195fa29dab0e7999246dd32
baoquoc <bquoc3002@gmail.com>
2023-12-09 01:49:58 +0700

    test commit

--- src/layouts/components/Header/index.js
+++ src/layouts/components/Header/index.js
@@ -29,7 +29,7 @@ const MENU_ITEMS = [
         icon: <FontAwesomeIcon icon={faEarthAsia} />,
         title: 'English',
         children: {
-            title: 'language',
+            title: 'Language',
             data: [
                 {
                     type: 'language',
