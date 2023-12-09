e635d6460383457725c40b7620acadc7c3506a27
baoquoc <bquoc3002@gmail.com>
2023-12-10 01:40:04 +0700

    update ui sidebar

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -8,10 +8,12 @@ import Following from '~/pages/Following';
 import Profile from '~/pages/Profile';
 import Upload from '~/pages/Upload';
 import Search from '~/pages/Search';
+import Live from '~/pages/Live';
 
 const publicRoutes = [
     { path: config.routers.home, component: Home },
     { path: config.routers.following, component: Following },
+    { path: config.routers.live, component: Live },
     { path: config.routers.profile, component: Profile },
     { path: config.routers.upload, component: Upload, layout: HeaderOnly },
     { path: config.routers.search, component: Search, layout: null },
